19dd1925557cce13b95453e4aa16fb94bd3e5f48
adil <adil.aslam1@icloud.com>
2024-07-14 13:21:54 +0500

    change info to infos

--- database/migrations/2024_07_13_183432_create_user_additional_info_table.php
+++ database/migrations/2024_07_13_183432_create_user_additional_info_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_additional_info', function (Blueprint $table) {
+        Schema::create('user_additional_infos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('phone');
